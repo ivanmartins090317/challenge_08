5e163b8a87e0884e06fa7cea29cc7ddafa5c7fd8
Ivan Martins <ivanmartins.ilha@gmail.com>
2024-08-26 10:41:01 -0300

    fix: ☑️resolution challenge 01

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react"
+
+
+
 const App = () =>{
+  const [image, setImage] = useState(null)
+
+  const handleClickImage = () =>{ 
+    fetch('https://dog.ceo/api/breeds/image/random')
+     .then(response => response.json())
+     .then(data => setImage(data))   
+  }
+  
   return (
     <div>
-      <p>Imagem_aleatoria</p>
-      <button>Buscar imagem</button>
+      <div>{image?.message && <img src={image.message} alt="Dogs aleatori" width="200px"/>}</div>
+      <button onClick={handleClickImage}>Buscar imagem</button>
     </div>
     
   )
